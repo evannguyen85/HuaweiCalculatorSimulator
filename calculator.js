64a082f0519db6288b19b145aa32b87847eb7cbc
Van The "Evan" Nguyen <42307234+evannguyen85@users.noreply.github.com>
2019-02-13 16:13:21 +0700

    calculator layout

---
+++ calculator.js
@@ -0,0 +1,18 @@
+function add(a, b) {
+	return a + b;
+}
+
+function minus(a, b) {
+	return a - b;
+}
+
+function multiply(a, b) {
+	return a * b;
+}
+
+function divide(a, b) {
+	return a / b;
+}
+
+var keypads = document.getElementsByClassName("keypad");
+console.log(keypads);
