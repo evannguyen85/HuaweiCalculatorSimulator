c2aff60ad1373e4e9bf38d5155c9ad64aadaa2f7
Van The "Evan" Nguyen <42307234+evannguyen85@users.noreply.github.com>
2019-02-13 17:45:45 +0700

    add eventlistener

--- calculator.js
+++ calculator.js
@@ -1,18 +1,21 @@
-function add(a, b) {
+function add(a, b, e) {
+	if (e.target !== e.currentTarget) {
+		var clickedItem = e.target.id;
+		console.log("clicked item: ");
+		console.log(clickedItem);
+		console.log("clicked item 2: ", clickedItem);
+	}
 	return a + b;
 }
 
-function minus(a, b) {
-	return a - b;
-}
 
-function multiply(a, b) {
-	return a * b;
-}
 
-function divide(a, b) {
-	return a / b;
-}
+var a = 5, b=10;
+var resultEle = document.getElementById("result");
+var equationEle = document.getElementById("equation");
+equationEle.innerHTML = a + " + " + b;
 
-var keypads = document.getElementsByClassName("keypad");
-console.log(keypads);
+var firstKeyRowEle = document.getElementById("key789m");
+firstKeyRowEle.addEventListener("click", function(e){
+	add(5, 6, e);
+}, false)
